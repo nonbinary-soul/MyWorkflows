0e9e501065461784c23ce1a6b1ec425dca9f1ba1
Jackie Lee Chong Ojeda <80453423+college-lee@users.noreply.github.com>
2024-04-26 19:54:02 +0200

    .mtl modificado y css añadido

---
+++ org.eclipse.acceleo.module.sample/workflow/database/Pizzeria.sql
@@ -0,0 +1,18 @@
+
+-- Creación de la base de datos
+CREATE DATABASE IF NOT EXISTS Pizzeria;
+
+-- Cambio al contexto de la base de datos
+USE Pizzeria;
+
+-- Creación de tablas
+-- Definición de la tabla tabla_clientes
+CREATE TABLE IF NOT EXISTS tabla_clientes (
+	dni INTEGER PRIMARY KEY AUTOINCREMENT, 
+	nombre TEXT, 
+	direccion TEXT, 
+	telefono TEXT, 
+	correo_electronico TEXT
+);
+
+
